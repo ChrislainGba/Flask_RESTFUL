18812775d9d1fd996087afe760f5ba388100d296
ChrislainGba <gbachrislain@gmail.com>
2020-05-22 12:47:14 +0000

    Added run.py

--- app.py
+++ app.py
@@ -12,10 +12,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key='Xl1'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 iwt = JWT(app, authenticate, identity)
 
 
